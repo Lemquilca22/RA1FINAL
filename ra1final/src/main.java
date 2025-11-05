316e88253e4e9d19ea52b4019ab5d9614fd93e1a
Lemsito22 <lemquilca22@gmail.com>
2025-11-05 09:23:15 +0100

    ...

---
+++ ra1final/src/main.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class main {
+    public static void main (String[] args) {
+        //Simulador de dados
+        Scanner sc = new Scanner(System.in);
+        Random generador = new Random();
+        System.out.println("Bienvenido al smiluador de dados");
+        System.out.println("Puedes usar 1 a 6 dados para jugar");
+        System.out.println("Con cuantos quieres jugar?");
+        int dados = sc.nextInt();
+
+        switch (dados) {
+            case 1:
+                i
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+        }
+    }
+}
