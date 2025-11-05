2b8669b50e36ea9b03653803c6ad280efb3889b6
Lemsito22 <lemquilca22@gmail.com>
2025-11-05 10:00:53 +0100

    ...

---
+++ ra1final/src/aleatorios.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class aleatorios {
+    public static void main (String[] args) {
+        //Simulador de dados
+        Scanner sc = new Scanner(System.in);
+        Random generador = new Random();
+        System.out.println("Bienvenido al generador de Números aleatorios");
+        System.out.println("Para poder jugar, debes escoger un rango, \n a continuación ingresa el valor minimo y maximo que puede tomar el número ");
+        System.out.println("Ingresa el valor min del numero");
+        int min = sc.nextInt();
+    }
+}
